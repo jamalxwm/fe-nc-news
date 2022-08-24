a726e0fe15a067d2efa873faaaae36b1c38a1706
Jamal Whorms-MonDesir <jamal@letswayfare.com>
2022-08-24 13:03:01 +0100

    Refactor articleCard buttons

--- src/components/ArticleList.jsx
+++ src/components/ArticleList.jsx
@@ -1,23 +1,20 @@
 import { useEffect, useState } from 'react';
 import { fetchArticles } from '../data-fetching';
-import { ThumbDown, ThumbUp } from '@mui/icons-material';
 import {
-  Button,
   CssBaseline,
   Card,
   CardMedia,
-  CardActions,
   CardContent,
   Grid,
   Container,
 } from '@material-ui/core';
 import { Typography } from '@mui/material';
 import useStyles from '../styles';
-import { useParams } from 'react-router';
+import { useParams, Link } from 'react-router-dom';
 
 export default function ArticleList() {
   const [articles, setArticles] = useState([]);
-  const {slug} = useParams();
+  const { slug } = useParams();
   const classes = useStyles();
 
   useEffect(() => {
@@ -33,26 +30,20 @@ export default function ArticleList() {
         <Grid container spacing={4}>
           {articles.map((article) => (
             <Grid item key={article.article_id} xs={12} sm={6} md={4}>
-              <Card className={classes.card}>
-                <CardMedia
-                  className={classes.cardMedia}
-                  image="https://source.unsplash.com/random"
-                  title={article.title}
-                />
-                <CardContent className={classes.cardContent}>
-                  <Typography gutterBottom variant="h5">
-                    {article.title}
-                  </Typography>
-                </CardContent>
-                <CardActions>
-                  <Button size="small" color="primary">
-                    <ThumbUp />
-                  </Button>
-                  <Button size="small" color="primary">
-                    <ThumbDown />
-                  </Button>
-                </CardActions>
-              </Card>
+              <Link to={`/articles/${article.article_id}`}>
+                <Card className={classes.card}>
+                  <CardMedia
+                    className={classes.cardMedia}
+                    image="https://source.unsplash.com/random"
+                    title={article.title}
+                  />
+                  <CardContent className={classes.cardContent}>
+                    <Typography gutterBottom variant="h5">
+                      {article.title}
+                    </Typography>
+                  </CardContent>
+                </Card>
+              </Link>
             </Grid>
           ))}
         </Grid>
--- src/components/ArticleView.jsx
+++ src/components/ArticleView.jsx
@@ -5,20 +5,22 @@ import { Typography } from '@mui/material';
 
 export default function ArticleView() {
   const { article_id } = useParams();
-  const [article, setArticle] = useState([]);
+  const [article, setArticle] = useState({});
 
   useEffect(() => {
-    fetchArticleByID(article_id).then((article) => setArticle(article));
+    fetchArticleByID(article_id).then((article) => {
+      setArticle(article[0]);
+    });
   }, [article_id]);
 
   return (
     <div>
-      <Typography variant='h2'>{article[0].title}</Typography>
-      <Typography variant='h6'>Author: {article[0].author}</Typography>
-      <Typography variant='h6'>Date: {article[0].created_at}</Typography>
-      <Typography variant='h6'>Votes: {article[0].votes}</Typography>
-      <Typography variant='body1'>{article[0].body}</Typography>
-      <Typography variant='h6'>Comments: {article[0].comment_count}</Typography>
+      <Typography variant="h2">{article.title}</Typography>
+      <Typography variant="h6">Author: {article.author}</Typography>
+      <Typography variant="h6">Date: {article.created_at}</Typography>
+      <Typography variant="h6">Votes: {article.votes}</Typography>
+      <Typography variant="body1">{article.body}</Typography>
+      <Typography variant="h6">Comments: {article.comment_count}</Typography>
     </div>
   );
 }
