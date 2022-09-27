9184efcf6f44afba81a03dedb355635740385a66
jamalxwm <jamal@letswayfare.com>
2022-09-28 00:04:00 +0100

    Remove div from routes

--- src/App.js
+++ src/App.js
@@ -12,7 +12,6 @@ function App() {
 
   return (
     <UserContext.Provider value={{ loggedInUser, setLoggedInUser }}>
-      <div className="App">
         <Routes>
           <Route path="/" element={<ArticleGrid />} />
           <Route path="/topic/:slug" element={<ArticleGrid />} />
@@ -23,7 +22,6 @@ function App() {
           />
           <Route path="/*" element={<Page404 />} />
         </Routes>
-      </div>
     </UserContext.Provider>
   );
 }
